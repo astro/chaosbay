1859482a2e7069252f1185ad7d5e73986cbb8e36
Astro <astro@spaceboyz.net>
2008-12-29 01:08:40 +0100

    sorted: fix

--- src/sorted.erl
+++ src/sorted.erl
@@ -1,12 +1,14 @@
 -module(sorted).
 
--export([new/3, to_list/1, insert/2]).
+-export([new/3, to_list/1, get_total/1, insert/2]).
+-export([compare/4]).
 
 
 -record(sorted, {n = 1,
 		 dir = asc,
 		 max = undefined,
-		 list = []}).
+		 list = [],
+		 total = 0}).
 
 new(N, Dir, Max) ->
     #sorted{n = N,
@@ -16,10 +18,14 @@ new(N, Dir, Max) ->
 to_list(#sorted{list = L}) ->
     L.
 
+get_total(#sorted{total = Total}) ->
+    Total.
+
 insert(#sorted{n = N,
 	       dir = Dir,
 	       max = Max,
-	       list = L}, E) ->
+	       list = L,
+	       total = Total} = S, E) ->
     L2 = insert1(N, Dir, L, E),
     L3 = if
 	     is_integer(Max) andalso length(L2) > Max ->
@@ -27,7 +33,7 @@ insert(#sorted{n = N,
 	     true ->
 		 L2
 	 end,
-    #sorted{list = L3}.
+    S#sorted{list = L3, total = Total + 1}.
 
 insert1(_, _, [], E) ->
     [E];
@@ -41,11 +47,11 @@ insert1(N, Dir, [L1 | L], E) ->
     end.
 
 compare(N, Dir, A, B) ->
-    A1 = element(N + 1, A),
-    B1 = element(N + 1, B),
+    A1 = element(N, A),
+    B1 = element(N, B),
     case Dir of
-	asc -> A1 < B1;
-	desc -> A1 > B1
+	desc -> A1 < B1;
+	asc -> A1 > B1
     end.
 
 	    
