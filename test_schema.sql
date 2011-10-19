e4ce7c8edc079ce29bc373bf3de878d59f9b6832
klObs <jw@klobs.de>
2011-10-20 01:07:50 +0200

    Postgres schema

---
+++ test_schema.sql
@@ -0,0 +1,33 @@
+-- script to create test schema for epgsql unit tests --
+--
+-- this script should be run as the same user the tests will be run as,
+-- so that the test for connecting as the 'current user' succeeds
+--
+-- the following lines must be added to pg_hba.conf for all tests to
+-- succeed:
+--
+-- host    epgsql_test_db1 epgsql_test             127.0.0.1/32    trust
+-- host    epgsql_test_db1 epgsql_test_md5         127.0.0.1/32    md5
+-- host    epgsql_test_db1 epgsql_test_cleartext   127.0.0.1/32    password
+-- hostssl epgsql_test_db1 epgsql_test_cert        127.0.0.1/32    cert
+--
+-- any 'trust all' must be commented out for the invalid password test
+-- to succeed.
+--
+-- ssl support must be configured, and the sslinfo contrib module
+-- loaded for the ssl tests to succeed.
+
+
+CREATE USER chaosbay;
+
+CREATE DATABASE chaosbaydb WITH TEMPLATE = 'template0' OWNER = 'chaosbay' ENCODING 'UTF8';
+
+GRANT ALL ON DATABASE chaosbaydb to chaosbay;
+
+\c chaosbaydb;
+
+CREATE TABLE torrents (infohash bigint primary key, name text, length integer, timestamp timestamp, data bytea);
+
+CREATE TABLE comments (id serial unique primary key, name text, timestamp timestamp default now(), comment text);
+
+-- CREATE TABLE peer (hash_peer, ip, port, downloaded, uploaded, left, upspeed, downspeed, last);
