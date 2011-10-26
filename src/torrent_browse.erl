932c49ff0a41fbc468a8c302a7d0a392bc7adf42
klObs <jw@klobs.de>
2011-10-27 00:25:57 +0200

    Sorting of torrents now works (it's a hack though).
    - Seeders, Leechers and Speed can not be sorted, as it is still in
      mnesia
    - epgsql does not accept order by $1 and neither accepts asc or desc in
      $1. So there is one big pattern matching function with hard coded
      sorting

--- src/torrent_browse.erl
+++ src/torrent_browse.erl
@@ -13,32 +13,83 @@
 % 	SortField = sort field as list. May contain name, length, age, comments, seeders, leechers, speed
 % 	SortDir = atom (asc, desc)
 % Output
-% 	Name (binary),
-% 	InfoHash (binary),
-% 	Length (integer),
-% 	Age (integer),
-% 	comments (integer),
-% 	seeders (integer),
-% 	leechers (integer),
-% 	speed (integer)
+%   list of #browse_result records
 search(Pattern, Max, Offset, SortField, SortDir) ->
 	io:format("search,Args: ~w ~w ~w ~w ~w~n",[Pattern, Max, Offset, SortField, SortDir]),
     Now = util:mk_timestamp(),
 	C = sql_conns:request_connection(),
-	{_, _, E} = pgsql:equery(C, "select (name, infohash, length, timestamp) from torrents", []),
+	MatchingTorrents = ordered_search(C, Pattern, Now, Max, Offset, SortField, SortDir),
+	sql_conns:release_connection(C),
 	Result = lists:flatmap(fun(X) -> 
-					{{Name, InfoHash, Length, Timestamp}} = X, 
-					{_, _, [{Comments}]} = pgsql:equery(C, "select count(*) from comments where name = $1", [Name]),
+					{{Name, InfoHash, Length, CommentCount, Age}} = X, 
 					[#browse_result{
 						name = Name,
 						id = InfoHash,
 						length = Length,
-						comments = Comments,
-						age = Now - Timestamp,
+						comments = CommentCount,
+						age = Age,
 						seeders = 0,
 						leechers = 0,
 						speed = 0}]
 				  end, 
-			E),
-	sql_conns:release_connection(C),
+			MatchingTorrents),
 	{Result, length(Result)}.
+
+% 	SortField = sort field as list. May contain name, length, age, comments, seeders, leechers, speed
+ordered_search(C, [], Now, Max, Offset, "name", asc) ->
+	{_, _, MatchingTorrents} = pgsql:equery(C, 
+		"select (name, infohash, length, count_comments(name), $1 - timestamp) from torrents order by name asc limit $2 offset $3", 
+		[Now, Max, Offset]),
+		MatchingTorrents;
+ordered_search(C, [], Now, Max, Offset, "name", desc) ->
+	{_, _, MatchingTorrents} = pgsql:equery(C, 
+		"select (name, infohash, length, count_comments(name), $1 - timestamp) from torrents order by name desc limit $2 offset $3", 
+		[Now, Max, Offset]),
+		MatchingTorrents;
+
+ordered_search(C, [], Now, Max, Offset, "length", asc) ->
+	{_, _, MatchingTorrents} = pgsql:equery(C, 
+		"select (name, infohash, length, count_comments(name), $1 - timestamp) from torrents order by length asc limit $2 offset $3", 
+		[Now, Max, Offset]),
+		MatchingTorrents;
+ordered_search(C, [], Now, Max, Offset, "length", desc) ->
+	{_, _, MatchingTorrents} = pgsql:equery(C, 
+		"select (name, infohash, length, count_comments(name), $1 - timestamp) from torrents order by length desc limit $2 offset $3", 
+		[Now, Max, Offset]),
+		MatchingTorrents;
+	
+ordered_search(C, [], Now, Max, Offset, "age", asc) ->
+	{_, _, MatchingTorrents} = pgsql:equery(C, 
+		"select (name, infohash, length, count_comments(name), $1 - timestamp) from torrents order by timestamp asc limit $2 offset $3", 
+		[Now, Max, Offset]),
+		MatchingTorrents;
+ordered_search(C, [], Now, Max, Offset, "age", desc) ->
+	{_, _, MatchingTorrents} = pgsql:equery(C, 
+		"select (name, infohash, length, count_comments(name), $1 - timestamp) from torrents order by timestamp desc limit $2 offset $3", 
+		[Now, Max, Offset]),
+		MatchingTorrents;
+
+ordered_search(C, [], Now, Max, Offset, "comments", asc) ->
+	{_, _, MatchingTorrents} = pgsql:equery(C, 
+		"select (name, infohash, length, count_comments(name), $1 - timestamp) from torrents order by count_comments(name) asc limit $2 offset $3", 
+		[Now, Max, Offset]),
+		MatchingTorrents;
+ordered_search(C, [], Now, Max, Offset, "comments", desc) ->
+	{_, _, MatchingTorrents} = pgsql:equery(C, 
+		"select (name, infohash, length, count_comments(name), $1 - timestamp) from torrents order by count_comments(name) desc limit $2 offset $3", 
+		[Now, Max, Offset]),
+		MatchingTorrents;
+
+ordered_search(C, [], Now, Max, Offset, _SortField, SortDir) ->
+	ordered_search(C, [], Now, Max, Offset, "name", SortDir);
+
+ordered_search(C, Pattern, Now, Max, Offset, "name", asc) ->
+			SearchPattern = "%" ++ Pattern ++ "%",	
+			{_, _, MatchingTorrents} = pgsql:equery(C, 
+				"select (name, infohash, length, count_comments(name) as comments, $1 - timestamp as age) from torrents where name ilike $2 order by name asc limit $3 offset $4", [Now, SearchPattern, Max, Offset]),
+			MatchingTorrents;
+ordered_search(C, Pattern, Now, Max, Offset, "name", desc) ->
+			SearchPattern = "%" ++ Pattern ++ "%",	
+			{_, _, MatchingTorrents} = pgsql:equery(C, 
+				"select (name, infohash, length, count_comments(name) as comments, $1 - timestamp as age) from torrents where name ilike $2 order by name desc limit $3 offset $4", [Now, SearchPattern, Max, Offset]),
+			MatchingTorrents.
